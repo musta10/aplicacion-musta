44dcb04ec8c3d1ca6c0f0ca5678feccf85765fca
musta10 <mustafaenfadal@gmail.com>
2025-03-27 01:10:58 +0000

    online-user

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,114 +1,157 @@
-const User = require('../models/User');
-const jwt = require('jsonwebtoken');
-const { validationResult } = require('express-validator');
+const User = require("../models/User");
+const jwt = require("jsonwebtoken");
+const { validationResult } = require("express-validator");
 
 // Generar JWT Token
 const generateToken = (user) => {
-    return jwt.sign({ id: user._id, role: user.role }, process.env.JWT_SECRET, {
-        expiresIn: '1h',
-    });
+  return jwt.sign({ id: user._id, role: user.role }, process.env.JWT_SECRET, {
+    expiresIn: "1h",
+  });
 };
 
 // Registro de Usuario
 exports.registerUser = async (req, res) => {
-    const errors = validationResult(req);
-    if (!errors.isEmpty()) {
-        return res.status(400).json({ errors: errors.array() });
+  const errors = validationResult(req);
+  if (!errors.isEmpty()) {
+    return res.status(400).json({ errors: errors.array() });
+  }
+
+  const { name, email, password, role, age, country } = req.body;
+
+  try {
+    let user = await User.findOne({ email });
+
+    if (user) {
+      return res.status(400).json({ msg: "El usuario ya existe" });
     }
 
-    const { name, email, password, role, age, country } = req.body;
+    user = new User({ name, email, password, role, age, country });
 
-    try {
-        let user = await User.findOne({ email });
+    await user.save();
 
-        if (user) {
-            return res.status(400).json({ msg: 'El usuario ya existe' });
-        }
+    const token = generateToken(user);
 
-        user = new User({ name, email, password, role , age , country });
-
-        await user.save();
-
-        const token = generateToken(user);
-
-        res.json({ token });
-    } catch (error) {
-        console.error(error.message);
-        res.status(500).send('Server error');
-    }
+    res.json({ token });
+  } catch (error) {
+    console.error(error.message);
+    res.status(500).send("Server error");
+  }
 };
 
 // Login de Usuario
 exports.loginUser = async (req, res) => {
-    const { email, password } = req.body;
+  const { email, password } = req.body;
 
-    try {
-        const user = await User.findOne({ email });
+  try {
+    const user = await User.findOne({ email });
 
-        if (!user) {
-            return res.status(400).json({ msg: 'Credenciales incorrectas' });
-        }
-
-        const isMatch = await user.matchPassword(password);
-
-        if (!isMatch) {
-            return res.status(400).json({ msg: 'Credenciales incorrectas' });
-        }
-
-        const token = generateToken(user);
-
-        res.json({ token });
-    } catch (error) {
-        console.error(error.message);
-        res.status(500).send('Server error');
+    if (!user) {
+      return res.status(400).json({ msg: "Credenciales incorrectas" });
     }
+
+    const isMatch = await user.matchPassword(password);
+
+    if (!isMatch) {
+      return res.status(400).json({ msg: "Credenciales incorrectas" });
+    }
+    // Actualizar el estado a online
+    user.isOnline = true;
+    await user.save();
+
+    const token = generateToken(user);
+
+    res.json({ token });
+  } catch (error) {
+    console.error(error.message);
+    res.status(500).send("Server error");
+  }
 };
+// Actualizar al cerrar la sesión
+exports.logoutUser = async (req, res) => {
+    try {
+      const user = await User.findById(req.user.id);
+  
+      if (!user) {
+        return res.status(404).json({ msg: "Usuario no encontrado" });
+      }
+  
+      // Actualizar el estado a offline
+      user.isOnline = false;
+      await user.save();
+  
+      res.json({ msg: "Cierre de sesión exitoso" });
+    } catch (error) {
+      console.error(error.message);
+      res.status(500).send("Server error");
+    }
+  };
+  
 // hay que verificar la ruta de la imagen con POSTMAN
 // Subir imagen de perfil
 exports.uploadProfileImage = async (req, res) => {
-    try {
-        if (!req.file) {
-            return res.status(400).json({ msg: 'Por favor sube una imagen' });
-        }
-
-        const user = await User.findById(req.user.id);
-
-        user.profileImage = req.file.path;  // Guardar la ruta de la imagen en el campo profileImage
-        await user.save();
-
-        res.json({ msg: 'Imagen subida correctamente', profileImage: user.profileImage });
-    } catch (error) {
-        console.error(error.message);
-        res.status(500).send('Server error');
+  try {
+    if (!req.file) {
+      return res.status(400).json({ msg: "Por favor sube una imagen" });
     }
-};
 
+    const user = await User.findById(req.user.id);
+
+    user.profileImage = req.file.path; // Guardar la ruta de la imagen en el campo profileImage
+    await user.save();
+
+    res.json({
+      msg: "Imagen subida correctamente",
+      profileImage: user.profileImage,
+    });
+  } catch (error) {
+    console.error(error.message);
+    res.status(500).send("Server error");
+  }
+};
 
 // Obtener todos los usuarios con paginación (solo para administradores)
 exports.getUsers = async (req, res) => {
     try {
-        const page = parseInt(req.query.page) || 1; // Página actual
-        const limit = parseInt(req.query.limit) || 10; // Número de usuarios por página
-
-        const skip = (page - 1) * limit;
-
-        // Obtener usuarios con paginación
-        const users = await User.find()
-            .select('-password') // Excluir el campo de contraseña
-            .skip(skip)
-            .limit(limit);
-
-        const totalUsers = await User.countDocuments(); // Contar total de usuarios
-        const totalPages = Math.ceil(totalUsers / limit); // Calcular páginas totales
-
-        res.json({
-            page,
-            totalPages,
-            totalUsers,
-            users
-        });
+      const page = parseInt(req.query.page) || 1;
+      const limit = parseInt(req.query.limit) || 10;
+      const skip = (page - 1) * limit;
+  
+      const users = await User.find()
+        .select("-password") // Excluir el campo de contraseña
+        .skip(skip)
+        .limit(limit);
+  
+      const totalUsers = await User.countDocuments();
+  
+      const formattedUsers = users.map((user) => ({
+        _id: user._id,
+        name: user.name,
+        email: user.email,
+        role: user.role,
+        age: user.age,
+        country: user.country,
+        profileImage: user.profileImage,
+        isOnline: user.isOnline, // Incluye el estado online
+        createdAt: user.createdAt
+          ? user.createdAt.toLocaleString("es-ES", {
+              day: "2-digit",
+              month: "2-digit",
+              year: "numeric",
+              hour: "2-digit",
+              minute: "2-digit",
+              second: "2-digit",
+            })
+          : "Fecha no disponible",
+      }));
+  
+      res.json({
+        page,
+        totalUsers,
+        totalPages: Math.ceil(totalUsers / limit),
+        users: formattedUsers,
+      });
     } catch (error) {
-        console.error(error);
-        res.status(500).send('Error en el servidor');
+      console.error(error.message);
+      res.status(500).send("Error en el servidor");
     }
-};
+  };
