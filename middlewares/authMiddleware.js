015d5f5e93786efe58709f07036a89a61ef2986c
musta10 <mustafaenfadal@gmail.com>
2025-03-27 01:16:09 +0000

    logout-user

--- middlewares/authMiddleware.js
+++ middlewares/authMiddleware.js
@@ -16,8 +16,7 @@ exports.protect = async (req, res, next) => {
     try {
         const decoded = jwt.verify(token, process.env.JWT_SECRET);
 
-        req.user = await User.findById(decoded.id);
-
+        req.user = await User.findById(decoded.id).select("-password");
         next();
     } catch (error) {
         res.status(401).json({ msg: 'Token inválido' });
